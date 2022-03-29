f96f7d05382d55f7d19316da6d21927fdd510cf7
janes.silva <janes_cleston@hotmail.com>
2022-03-29 18:54:09 -0300

    Atualização

--- api-back-end/APIs/index.js
+++ api-back-end/APIs/index.js
@@ -3,3 +3,17 @@ const express = require('express')
 const app = express()
 
 app.listen('3000')
+
+//Para acessos GET
+// app.route('/').get((req, res) => res.send("index"))
+// app.route('/pagina').get((req, res) => res.send("página"))
+//-------------------------------------------------------
+
+
+//Para acesso POST
+//middleware
+app.use(express.json())
+
+// app.route('/').post((req, res) => console.log(req.body))
+app.route('/').post((req, res) => res.send(req.body))
+//-------------------------------------------------------
