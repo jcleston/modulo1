07c84fd3fce8e2a6034ecd10399e74293d151927
jcleston <janes.cleston.silva@gmail.com>
2022-04-04 13:03:55 -0300

    Atualização

--- fetch-discover-api/main.js
+++ fetch-discover-api/main.js
@@ -2,6 +2,9 @@ const url="http://localhost:5500/api"
 
 function getUsers(){
     fetch(url)
-    .then(response => console.log(response.json))
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
     .catch(error => console.error(error))
 }
+
+getUsers()
